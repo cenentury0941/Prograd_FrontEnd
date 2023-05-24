50c3578f65e2ace6a0c4b432f7ff5a82ebb6ec7e
unknown <cenentury0941@gmail.com>
2023-05-24 13:06:37 +0530

    Carousal Movement Functionality Added

--- Carousal/assets/js/script.js
+++ Carousal/assets/js/script.js
@@ -1,1 +1,38 @@
-console.log("Hello WOrld");
+var curpos = 0 ;
+var maxpos = 400;
+var minpos = 0;
+
+function PreviousImage()
+{
+   console.log("clicked");
+   if( curpos > minpos )
+   {
+    curpos -= 100;
+   }
+   var elems = document.getElementsByClassName("Carousel-Item");
+   for( var elem of elems )
+   {
+      elem.className = "Carousel-Item img1 p" + curpos;
+   }
+}
+
+
+function NextImage()
+{
+   console.log("clicked");
+   if( curpos < maxpos )
+   {
+    curpos += 100;
+   }
+   var elems = document.getElementsByClassName("Carousel-Item");
+   for( var elem of elems )
+   {
+      elem.className = "Carousel-Item img1 p" + curpos;
+   }
+}
+
+
+function ChangeColour()
+{
+
+}
